36e382d8e03bcf9ef11cefae69be43650237376c
RyzzBot <115353214+RyzzBot@users.noreply.github.com>
2022-10-28 15:13:29 +0700

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -31,7 +31,7 @@ global.botname = 'THE RAYZO BOT' //ubah jadi nama bot mu, note tanda ' gausah di
 global.footer = 'Rayzo' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6283837166690'] // ubah aja pake nomor lu
+global.owner = ['6283837166690','6283111514019'] // ubah aja pake nomor lu
 global.premium = ['6285776191421'] // ubah aja pake nomor lu
 global.packname = 'THE RAYZO BOT' // ubah aja
 global.ownerr = ['Rayzo177'] // ubaha aja
